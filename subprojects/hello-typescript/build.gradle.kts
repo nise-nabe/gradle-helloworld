71657f653b3a22aabb1f3d36fcaa2757ad433801
nise_nabe <nise.nabe@gmail.com>
2020-12-17 13:11:55 +0900

    add opt in compiler option for js export

--- subprojects/hello-typescript/build.gradle.kts
+++ subprojects/hello-typescript/build.gradle.kts
@@ -3,12 +3,19 @@ plugins {
 }
 
 
-kotlin {
-    js(IR) {
-        browser()
-    }
-}
 
 dependencies {
     testImplementation(kotlin("test-js"))
+}
+
+
+kotlin {
+    js(IR) {
+        browser()
+        compilations.all {
+            compileKotlinTask.kotlinOptions.freeCompilerArgs += listOf(
+                "-Xopt-in=kotlin.js.ExperimentalJsExport"
+            )
+        }
+    }
 }
