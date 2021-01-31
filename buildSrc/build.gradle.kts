dd17c615406cab221802f99d35bb5461f0e14f90
nise_nabe <nise.nabe@gmail.com>
2021-01-31 23:49:51 +0900

    spring boot 2.4.1 -> 2.4.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-allopen")
     implementation("org.jetbrains.kotlin:kotlin-noarg")
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.1")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.2")
 
     implementation("com.diffplug.spotless:spotless-plugin-gradle:5.8.2")
 }
