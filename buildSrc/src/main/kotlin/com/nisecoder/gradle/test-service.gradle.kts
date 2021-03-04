67b85b47c6b20e61247f4b5e3ac9b6c13fc0a64f
nise_nabe <nise.nabe@gmail.com>
2021-03-04 19:55:19 +0900

    add test service demo

---
+++ buildSrc/src/main/kotlin/com/nisecoder/gradle/test-service.gradle.kts
@@ -0,0 +1,10 @@
+package com.nisecoder.gradle
+
+import org.gradle.kotlin.dsl.support.serviceOf
+import org.gradle.tooling.events.OperationCompletionListener
+
+
+serviceOf<BuildEventsListenerRegistry>()
+     .onTaskCompletion(provider { OperationCompletionListener {
+    logger.lifecycle("${it.descriptor.name}:finish")
+}})
