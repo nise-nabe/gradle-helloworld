c666b452bbe676a702737941747c851ddb92ac19
nise_nabe <nise.nabe@gmail.com>
2021-03-04 21:54:27 +0900

    add kotlin convention plugin

---
+++ buildSrc/src/main/kotlin/com/nisecoder/gradle/kotlin.gradle.kts
@@ -0,0 +1,18 @@
+package com.nisecoder.gradle
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+val compiler: Provider<JavaCompiler> = javaToolchains.compilerFor {
+    languageVersion.set(JavaLanguageVersion.of(15))
+    vendor.set(JvmVendorSpec.ADOPTOPENJDK)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_15.toString()
+    }
+}
