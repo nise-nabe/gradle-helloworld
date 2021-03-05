6486ded4e980c5ba3fa27ee3aa8fcbd6964be036
nise_nabe <nise.nabe@gmail.com>
2021-03-05 10:03:05 +0900

    kotlin libs to use 1.4 and 1.4.31

--- buildSrc/src/main/kotlin/com/nisecoder/gradle/kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/com/nisecoder/gradle/kotlin.gradle.kts
@@ -13,10 +13,13 @@ val compiler: Provider<JavaCompiler> = javaToolchains.compilerFor {
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
+        languageVersion = "1.4"
         jvmTarget = JavaVersion.VERSION_15.toString()
     }
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.4.31"))
+
     testImplementation(kotlin("test-junit5"))
 }
