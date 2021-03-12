19fb30f29c02733ea469f0137b2453a8d4afdce9
nise_nabe <nise.nabe@gmail.com>
2021-03-12 09:59:38 +0900

    add spring repo to resolve snapshot

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,5 +1,29 @@
 pluginManagement {
     repositories {
+        maven {
+            name = "Spring Plugin Release Repository"
+            url = uri("https://repo.spring.io//plugins-release/")
+            mavenContent {
+                releasesOnly()
+                includeGroup("org.springframework.boot")
+            }
+            metadataSources {
+                gradleMetadata()
+            }
+        }
+
+        maven {
+            name = "Spring Plugin Snapshot Repository"
+            url = uri("https://repo.spring.io//plugins-snapshot/")
+            mavenContent {
+                snapshotsOnly()
+                includeGroup("org.springframework.boot")
+            }
+            metadataSources {
+                gradleMetadata()
+            }
+        }
+
         gradlePluginPortal()
     }
 }
