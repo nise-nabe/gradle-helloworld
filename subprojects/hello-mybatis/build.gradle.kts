32c36713fb647a925d32179438eb5b0bcee1cd79
nise_nabe <nise.nabe@gmail.com>
2021-03-13 17:38:10 +0900

    add test container to test mysql and use junit5

--- subprojects/hello-mybatis/build.gradle.kts
+++ subprojects/hello-mybatis/build.gradle.kts
@@ -8,4 +8,11 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.+")
+
+    testImplementation("org.testcontainers:testcontainers:1.+")
+    testImplementation("org.testcontainers:junit-jupiter:1.+")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
