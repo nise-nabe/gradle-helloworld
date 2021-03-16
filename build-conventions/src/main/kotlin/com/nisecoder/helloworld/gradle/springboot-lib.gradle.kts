62c5187e872cdf1c19815fb2c493f5522af22143
nise_nabe <nise.nabe@gmail.com>
2021-03-16 10:35:24 +0900

    add springboot sourceSet feature

--- build-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/springboot-lib.gradle.kts
+++ build-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/springboot-lib.gradle.kts
@@ -7,6 +7,31 @@ plugins {
     id("com.nisecoder.helloworld.gradle.springboot-base")
 }
 
+sourceSets {
+    create("springboot") {
+        val mainSourceSet = sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
+        val mainOutput = objects.fileCollection().from(mainSourceSet.output)
+
+        compileClasspath += mainOutput
+        runtimeClasspath += mainOutput
+
+        configurations.getByName(implementationConfigurationName)
+                .extendsFrom(configurations.getByName(mainSourceSet.implementationConfigurationName))
+        configurations.getByName(apiConfigurationName)
+                .extendsFrom(configurations.getByName(mainSourceSet.apiConfigurationName))
+    }
+}
+
+java {
+    registerFeature("springboot") {
+        usingSourceSet(sourceSets["springboot"])
+    }
+}
+
 dependencies {
     api(platform(SpringBootPlugin.BOM_COORDINATES))
+
+    val springbootApi by configurations.getting {}
+    springbootApi(platform(SpringBootPlugin.BOM_COORDINATES))
+    springbootApi("org.springframework.boot:spring-boot-starter")
 }
