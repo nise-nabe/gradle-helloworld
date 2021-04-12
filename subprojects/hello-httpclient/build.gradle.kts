92aaecaa613e6b0d8e702e07962418517d43193c
nise_nabe <nise.nabe@gmail.com>
2021-04-12 09:27:24 +0900

    add http client variation

--- subprojects/hello-httpclient/build.gradle.kts
+++ subprojects/hello-httpclient/build.gradle.kts
@@ -5,11 +5,21 @@ plugins {
 }
 
 dependencies {
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.+")) {
+    }
 
     implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.+")
 
     testImplementation("com.squareup.okhttp3:mockwebserver")
-    testImplementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
+    testImplementation("com.squareup.retrofit2:retrofit-mock:2.+")
+}
+
+dependencies {
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.0")
+}
+
+dependencies {
+    implementation(platform("com.linecorp.armeria:armeria-bom:1.6.+"))
+    implementation("com.linecorp.armeria:armeria")
 }
