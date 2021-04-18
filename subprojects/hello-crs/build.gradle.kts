85f318b9827ade94b8048cfe472c34b616a53569
nise_nabe <nise.nabe@gmail.com>
2021-04-18 11:53:19 +0900

    hello-crs to use kotlin

--- subprojects/hello-crs/build.gradle.kts
+++ subprojects/hello-crs/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     `java-library`
     id("com.nisecoder.helloworld.gradle.build-basic")
+    id("com.nisecoder.helloworld.gradle.kotlin")
 }
 
+dependencies {
+    implementation("org.redundent:kotlin-xml-builder:1.7.2")
+}
