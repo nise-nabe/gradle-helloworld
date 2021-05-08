daf01014d1710b5e517de6eb426dae4202d38adf
nise_nabe <nise.nabe@gmail.com>
2021-05-08 13:13:10 +0900

    add document for plugin dependencies

--- build-conventions/build.gradle.kts
+++ build-conventions/build.gradle.kts
@@ -14,22 +14,32 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-allopen")
     implementation("org.jetbrains.kotlin:kotlin-noarg")
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.+")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.+") {
+        because("springboot convention plugins")
+    }
 
     implementation("com.diffplug.spotless:spotless-plugin-gradle:5.+")
 
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.+")
+    implementation("com.google.protobuf:protobuf-gradle-plugin:0.+") {
+        because("protobuf and grpc convention plugin")
+    }
 
     implementation("com.android.tools.build:gradle:4.1.3")
 
-    implementation("gradle.plugin.com.github.bjornvester:xjc-gradle-plugin:1.6.0")
+    implementation("gradle.plugin.com.github.bjornvester:xjc-gradle-plugin:1.6.0") {
+        because("xsd convention plugin")
+    }
 
-    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-gradle:4.6.0")
+    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-gradle:4.6.0") {
+        because("graphql convention plugin")
+    }
 
-    implementation("com.github.node-gradle:gradle-node-plugin:3.1.0")
+    implementation("com.github.node-gradle:gradle-node-plugin:3.1.0") {
+        because("nodejs convention plugin")
+    }
 
     implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.3.2") {
-        because("asciidoc.gradle.kts")
+        because("asciidoc convention plugin")
     }
 }
 
