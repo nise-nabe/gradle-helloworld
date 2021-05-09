48b88fbfed25e36ac74ce2eba8f4bf9fca59eaed
nise_nabe <nise.nabe@gmail.com>
2021-05-09 21:32:04 +0900

    add analyze method

--- subprojects/hello-nuxtjs/nuxt.config.ts
+++ subprojects/hello-nuxtjs/nuxt.config.ts
@@ -1,8 +1,15 @@
 import { NuxtConfig } from "@nuxt/types";
 
+const isProd = process.env.NODE_ENV !== 'production'
+
 const config: NuxtConfig = {
     target: "static",
-    dev: process.env.NODE_ENV !== 'production',
+    dev: !isProd,
+    build: {
+        analyze: {
+            analyzerMode: isProd ? "static" : "disabled"
+        }
+    }
 }
 
 export default config
