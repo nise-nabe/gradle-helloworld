e3d712e7fbdfcb9a2aebed4dc753026070fdc031
nise_nabe <nise.nabe@gmail.com>
2021-07-15 10:21:00 +0900

    add test ng testcase

---
+++ subprojects/hello-testng/src/test/kotlin/com/nisecoder/helloworld/HelloTestNg.kt
@@ -0,0 +1,11 @@
+package com.nisecoder.helloworld
+
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+
+class HelloTestNg {
+    @Test
+    fun test() {
+        assertEquals("hello", "hello")
+    }
+}
