12ecf39c824d1509f8eccbd9bcb1736de2045dab
nise_nabe <nise.nabe@gmail.com>
2021-08-08 23:17:30 +0900

    fix to use jetbrains runtime to develop intelli idea plugin

--- build-conventions/java-plugin-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/IntellijGradlePlugin.kt
+++ build-conventions/java-plugin-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/IntellijGradlePlugin.kt
@@ -3,11 +3,7 @@ package com.nisecoder.helloworld.gradle
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.jvm.toolchain.JavaToolchainService
-import org.gradle.jvm.toolchain.JvmVendorSpec
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.intellij.IntelliJPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -15,19 +11,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 class IntellijGradlePlugin: Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         plugins.apply(IntelliJPlugin::class)
-        val compiler = extensions.getByName<JavaToolchainService>("javaToolchains") .compilerFor {
-            @Suppress("MagicNumber")
-            languageVersion.set(JavaLanguageVersion.of(11))
-            vendor.set(JvmVendorSpec.ADOPTOPENJDK)
-        }
+
         tasks.withType<KotlinCompile>().configureEach {
             kotlinOptions {
                 languageVersion = "1.5"
                 apiVersion = "1.5"
                 jvmTarget = JavaVersion.VERSION_11.toString()
                 javaParameters = true
-
-                jdkHome = compiler.get().metadata.installationPath.asFile.absolutePath
             }
         }
     }
