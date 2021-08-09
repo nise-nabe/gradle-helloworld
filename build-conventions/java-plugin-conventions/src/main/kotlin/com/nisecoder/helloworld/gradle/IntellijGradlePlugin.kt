15e76e352c81f6e254bea2d3f197b80a9702a47d
nise_nabe <nise.nabe@gmail.com>
2021-08-09 18:02:04 +0900

    define default value in plugin

--- build-conventions/java-plugin-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/IntellijGradlePlugin.kt
+++ build-conventions/java-plugin-conventions/src/main/kotlin/com/nisecoder/helloworld/gradle/IntellijGradlePlugin.kt
@@ -4,8 +4,11 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.intellij.IntelliJPlugin
+import org.jetbrains.intellij.IntelliJPluginExtension
+import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class IntellijGradlePlugin: Plugin<Project> {
@@ -19,6 +22,21 @@ class IntellijGradlePlugin: Plugin<Project> {
                 jvmTarget = JavaVersion.VERSION_11.toString()
                 javaParameters = true
             }
+        }
+
+        extensions.configure<IntelliJPluginExtension>("intellij") {
+            version.set("IC-2021.2")
+            configureDefaultDependencies.set(false)
+
+            type.set("IC")
+            downloadSources.set(true)
+            updateSinceUntilBuild.set(true)
+            sameSinceUntilBuild.set(true)
+        }
+
+        tasks.named<RunIdeTask>("runIde") {
+            // avoid warning log
+            jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
         }
     }
 }
--- subprojects/intellij-idea-plugin/hello-intellij-idea-plugin-simple/build.gradle.kts
+++ subprojects/intellij-idea-plugin/hello-intellij-idea-plugin-simple/build.gradle.kts
@@ -6,17 +6,5 @@ plugins {
 version = "1.0-SNAPSHOT"
 
 intellij {
-    version.set("IC-2021.2")
-    configureDefaultDependencies.set(false)
-
     pluginName.set("hello world")
-    type.set("IC")
-    downloadSources.set(true)
-    updateSinceUntilBuild.set(true)
-    sameSinceUntilBuild.set(true)
-}
-
-tasks.runIde {
-    // avoid warning log
-    jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
 }
