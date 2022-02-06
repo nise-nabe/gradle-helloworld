355b9341feeea9f2c6a3232c975efaf06a708e16
nise_nabe <nise.nabe@gmail.com>
2022-02-06 21:58:30 +0900

    avoid each download for released SNAPSHOT version

--- subprojects/hello-minecraft-spigot-mod/build.gradle.kts
+++ subprojects/hello-minecraft-spigot-mod/build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
 }
+
+configurations.all {
+    resolutionStrategy {
+        cacheDynamicVersionsFor(7, "days")
+    }
+}
