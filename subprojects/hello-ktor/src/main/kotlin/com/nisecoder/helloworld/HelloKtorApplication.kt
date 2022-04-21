db51c14f21ea7a3ef07800ae35d951b3ce24d3dc
nise_nabe <nise.nabe@gmail.com>
2022-04-21 19:38:47 +0900

    rename

--- subprojects/hello-ktor/src/main/kotlin/com/nisecoder/helloworld/HelloKtorApplication.kt
+++ subprojects/hello-ktor/src/main/kotlin/com/nisecoder/helloworld/HelloKtorApplication.kt
@@ -10,11 +10,11 @@ import io.ktor.server.routing.routing
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
+        module()
     }.start(wait = true)
 }
 
-fun Application.configureRouting() {
+fun Application.module() {
     routing {
         get("/") {
             call.respondText("Hello, world!")
