4d793170badee403801e7f8a1bc9db3e9c269df3
nise_nabe <nise.nabe@gmail.com>
2022-06-18 11:58:52 +0900

    spring boot 2.6 -> 2.7

--- build-logic/spring-boot-plugins/build.gradle.kts
+++ build-logic/spring-boot-plugins/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-allopen")
     implementation("org.jetbrains.kotlin:kotlin-noarg")
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.+") {
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.0") {
         because("springboot convention plugins")
     }
 }
