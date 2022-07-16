745e81fbf18f4f594a2e981190714ce0ea506e7d
nise_nabe <nise.nabe@gmail.com>
2022-07-16 09:47:05 +0900

    use fixed protobuf gradle plugin vresion

--- build-logic/kotlin-dsl-plugins/build.gradle.kts
+++ build-logic/kotlin-dsl-plugins/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.+") {
+    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.18") {
         because("protobuf and grpc convention plugin")
     }
 }
