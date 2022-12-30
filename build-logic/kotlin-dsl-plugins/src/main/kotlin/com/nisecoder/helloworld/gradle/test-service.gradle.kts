a2975eea56e8355c3e5ae08f4b81a77ca0a3d1ab
nise_nabe <nise.nabe@gmail.com>
2022-12-30 19:25:01 +0900

    impl build service to avoid configuration cache issue

--- build-logic/kotlin-dsl-plugins/src/main/kotlin/com/nisecoder/helloworld/gradle/test-service.gradle.kts
+++ build-logic/kotlin-dsl-plugins/src/main/kotlin/com/nisecoder/helloworld/gradle/test-service.gradle.kts
@@ -1,10 +1,14 @@
 package com.nisecoder.helloworld.gradle
 
 import org.gradle.kotlin.dsl.support.serviceOf
+import org.gradle.tooling.events.FinishEvent
 import org.gradle.tooling.events.OperationCompletionListener
 
 
-serviceOf<BuildEventsListenerRegistry>()
-     .onTaskCompletion(provider { OperationCompletionListener {
-    logger.debug("${it.descriptor.name}:finish")
-}})
+serviceOf<BuildEventsListenerRegistry>().onTaskCompletion(provider { MyListener() })
+
+class MyListener: OperationCompletionListener {
+    override fun onFinish(event: FinishEvent) {
+        logger.debug("${event.descriptor.name}:finish")
+    }
+}
