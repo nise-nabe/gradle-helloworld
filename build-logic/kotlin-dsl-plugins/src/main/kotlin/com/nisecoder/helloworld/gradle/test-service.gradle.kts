821b4737590c9ff87d0da17327d06aa8872ae5ed
nise_nabe <nise.nabe@gmail.com>
2022-12-30 19:29:16 +0900

    avoid to access project on listener  (logger)

--- build-logic/kotlin-dsl-plugins/src/main/kotlin/com/nisecoder/helloworld/gradle/test-service.gradle.kts
+++ build-logic/kotlin-dsl-plugins/src/main/kotlin/com/nisecoder/helloworld/gradle/test-service.gradle.kts
@@ -1,14 +1,9 @@
 package com.nisecoder.helloworld.gradle
 
 import org.gradle.kotlin.dsl.support.serviceOf
-import org.gradle.tooling.events.FinishEvent
 import org.gradle.tooling.events.OperationCompletionListener
 
 
-serviceOf<BuildEventsListenerRegistry>().onTaskCompletion(provider { MyListener() })
-
-class MyListener: OperationCompletionListener {
-    override fun onFinish(event: FinishEvent) {
-        logger.debug("${event.descriptor.name}:finish")
-    }
-}
+serviceOf<BuildEventsListenerRegistry>().onTaskCompletion(provider { OperationCompletionListener {
+//    println("${it.descriptor.name}:finish")
+}})
